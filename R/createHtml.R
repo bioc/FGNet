1c2f0ffc18190c0a29d2459da36b9eb771151b9b
Sara Aibar <saibar@usal.es>
2014-03-14 13:41:44 +0000

    Create html
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_13/madman/Rpacks/FGNet@87451 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/createHtml.R
+++ R/createHtml.R
@@ -144,22 +144,29 @@ buildTermsTable <- function(termsDescriptions)
 creteGoLinks <- function(goIds, folder)
 {
 	goTerms <- sapply(goIds, function(x) if(length(x)>0) paste("%22GO%3A", x,"%22%3A{%22fill%22%3A%22%23ccccff%22}", sep="",collapse=","))
-	goLinks <- paste("http://amigo.geneontology.org/cgi-bin/amigo/visualize?mode=advanced&term_data={",goTerms ,"}&term_data_type=json&format=png", sep="")
-	names(goLinks) <- names(goIds)
-	
-	fileNames <- paste(folder, paste("GO_", gsub("s ","_",names(goIds)),".png", sep=""),sep="")
-	names(fileNames) <- names(goIds)
-	
-	for(i in 1:length(goLinks)) 
+	if(length(goTerms)>0)
 	{
-		fileNames[i] <- tryCatch({
-				download.file(url=goLinks[i], destfile=fileNames[i], quiet=TRUE)
-				return(fileNames[i]) # se ha descargado bien: linkar
-		}, error = function(e) {
-			  return(goLinks[i]) # No se ha podido descargar. Link a la url
-		})
+		goLinks <- paste("http://amigo.geneontology.org/cgi-bin/amigo/visualize?mode=advanced&term_data={",goTerms ,"}&term_data_type=json&format=png", sep="")
+		names(goLinks) <- names(goIds)
+		
+		fileNames <- paste(folder, paste("GO_", gsub("s ","_",names(goIds)),".png", sep=""),sep="")
+		names(fileNames) <- names(goIds)
+		
+		for(i in 1:length(goLinks)) 
+		{
+			fileNames[i] <- tryCatch({
+					download.file(url=goLinks[i], destfile=fileNames[i], quiet=TRUE)
+					return(fileNames[i]) # se ha descargado bien: linkar
+			}, error = function(e) {
+					if (file.exists(fileNames[i])) file.remove(fileNames[i])
+				  return(goLinks[i]) # No se ha podido descargar. Link a la url
+			})
+		}
+		return(fileNames)
+	} else 
+	{
+		return("")
 	}
-	return(fileNames)
 }
 
 
