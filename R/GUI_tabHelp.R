4ac64d381b160e8f2a8f9c1326aa41458e1f4082
Sara Aibar <saibar@usal.es>
2014-10-07 09:34:43 +0000

    2.99.1 - Minor help improvements.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/FGNet@95049 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/GUI_tabHelp.R
+++ R/GUI_tabHelp.R
@@ -14,10 +14,11 @@ showVignette <- function(button) # button: Not used, but required for signalConn
  
 tabHelp_fill <- function(mainWindow)
 {
-    tabHelp <- gtkVBox(FALSE,3)
+    tabHelp <- gtkHBox(FALSE,10)
+    tabHelpV <- gtkVBox(FALSE,10)
     
-    
-    
+    helpTxt <- 'To generate the functional network:\n\n 1. Run the Functional Enrichment Analisis (FEA) with one of the tools (Tab "1 - FEA")\n\n 2. Generate the HTML report or personalize the networks (Tab "2 - Network")\n'
+    tabHelpV$packStart(gtkLabel(helpTxt), TRUE, TRUE, 50)
     
     vignetteFrame <- gtkFrame("To see FGNet tutorial, type in R console:")
     gtkFrameSetShadowType(vignetteFrame, GtkShadowType["none"])
@@ -25,7 +26,9 @@ tabHelp_fill <- function(mainWindow)
     vignetteText$setText('browseVignettes("FGNet")')
     vignetteFrame$add(vignetteText)
     
-    tabHelp$packStart(vignetteFrame, FALSE, FALSE, 0)
+    tabHelpV$packStart(vignetteFrame, FALSE, FALSE, 50)
+    
+    tabHelp$packStart(tabHelpV, FALSE, FALSE, 50)
     
 #     buttonVignette <- gtkLabel("User guide")
 #     tabHelp$packStart(buttonVignette, FALSE, FALSE, 0)
